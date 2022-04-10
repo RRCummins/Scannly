3d2306ad97067243b82755f8c6f921fc937403ed
Ryan Cummins <brokenrhino@gmail.com>
2022-04-10 12:42:51 -0400

    Added a favorite items list

--- Scannly/Scannly/ContentView.swift
+++ Scannly/Scannly/ContentView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct ContentView: View {
     @ObservedObject var scanner = BTScanner()
     
+    @State private var favorites = [CBPeripheral]()
     @State private var isScanning = false
     
     var body: some View {
@@ -20,15 +21,39 @@ struct ContentView: View {
                     .font(.system(size: 36, weight: .bold, design: .default))
                     .foregroundStyle(.linearGradient(Gradient(colors: gradientColors), startPoint: .leading, endPoint: .trailing))
                     .padding()
-                VStack {
-                    List(scanner.peripherals, id: \.self) { peri in
-                        NavigationLink {
-                            PeripheralView(peripheral: peri)
-                        } label: {
-                            Text(peri.name ?? "Unnamed")
+                List {
+                    Section("Favorites") {
+                        ForEach(favorites, id: \.self) { favorite in
+                            NavigationLink {
+                                PeripheralView(peripheral: favorite)
+                            } label: {
+                                Text(favorite.name ?? "Unnamed")
+                            }
+                            .swipeActions(edge: .trailing, allowsFullSwipe: true) {
+                                Button {
+                                    favorites.removeAll(where: {$0 == favorite})
+                                } label: {
+                                    Label("Unfavorite", image: "star.slash")
+                                }
+                            }
                         }
                     }
-                    .listStyle(.insetGrouped)
+                    Section("Discovered") {
+                        ForEach(scanner.peripherals, id: \.self) { peripheral in
+                            NavigationLink {
+                                PeripheralView(peripheral: peripheral)
+                            } label: {
+                                Text(peripheral.name ?? "Unnamed")
+                            }
+                            .swipeActions(edge: .trailing, allowsFullSwipe: true) {
+                                Button {
+                                    favorites.append(peripheral)
+                                } label: {
+                                    Label("Favorite", image: "star")
+                                }
+                            }
+                        }
+                    }
                 }
             }
             .background(Color.init(UIColor.systemGroupedBackground))
