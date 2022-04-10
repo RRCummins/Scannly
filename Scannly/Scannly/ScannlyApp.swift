d09ab49ddb17463e9b629fa9d2aa7af423b9c8d3
Ryan Cummins <brokenrhino@gmail.com>
2022-04-10 11:11:45 -0400

    Scannly init

---
+++ Scannly/Scannly/ScannlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScannlyApp.swift
+//  Scannly
+//
+//  Created by Ryan Cummins on 4/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ScannlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
